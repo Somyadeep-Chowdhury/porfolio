429864894244ffe768adec970a36f3579bf03045
Soumyadeep Chowdhury <soumyadeep123456@gmail.com>
2021-01-23 09:09:18 +0000

    first commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Somyadeep Chowdhury",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Somyadeep Chowdhury and I'm a Software Engineer!",
+  githubUrl: "https://github.com/Somyadeep-Chowdhury",
+  linkedinUrl: "https://www.linkedin.com/in/somyadeep-chowdhury-a23448149",
+  cvLink: "https://drive.google.com/file/d/1qgtXvGY1zzQKfthlXFk-x01NHhQQBtzI/view?usp=sharing",
+};
